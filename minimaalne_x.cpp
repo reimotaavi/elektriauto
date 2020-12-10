3e59f675fe9cc9d06f0133cf2e85c95251b2b83f
uqbit <74680333+uqbit@users.noreply.github.com>
2020-12-10 19:35:48 +0200

    teeb maatriksi csv failist

--- minimaalne_x.cpp
+++ minimaalne_x.cpp
@@ -6,29 +6,46 @@
 
 using namespace std;
 
-int linnade_arv = 59;
 
 void loe_andmed(const string& failinimi){
 
+    int linnade_arv = 59;
     vector<vector<int>> kauguste_maatriks;
 
     ifstream sisend(failinimi);
     sisend.ignore(1000, '\n'); //ignoreerin esimest rida
-    while(sisend.is_open()){
+    if(sisend.is_open()){
         string rida;
-        vector<int> kaugused;
+        string arv = "";
+        int kaugus = 0;
         for(unsigned int i = 0; i < linnade_arv; i++){
-            for(unsigned int j = 0; j <= linnade_arv; j++){
-                getline(sisend, rida, ',');
-                if(j != 0){
-                    int kaugus = stoi(rida);
-                    cout << kaugus;
-                    kaugused[j - 1] = kaugus;
+            vector<int> kaugused;
+            sisend.ignore(256, ','); // ignoreerin linna nime
+            getline (sisend, rida, '\n'); // loen terve rea sisse
+            rida += ","; // lisan rea loppu koma, et saaks hiljem ka viimase kauguse kätte
+
+            for (unsigned int j = 0; j<rida.size(); j++) {
+                if (rida[j] != ',') {           // kui antud kohal ei leidu koma, siis lisan selle tähe uude stringi
+                    arv += rida[j];
+                }
+                else {
+                    kaugus = stoi(arv); // kui koma leidub siis panen olemasoleva stringi arvuna vectorisse ja algväärtustan string
+                    kaugused.push_back(kaugus);
+                    arv = "";
                 }
             }
-            cout << endl;
-            kauguste_maatriks[i] = kaugused;
+                kauguste_maatriks.push_back(kaugused); // lisan vastava linna kõik kaugused vectorisse
         }
+        // kuvan maatriksi vektori kontrolliks
+        for (unsigned int i = 0; i<kauguste_maatriks.size(); i++) {
+            cout << endl << endl << i+1 << ". linna kaugused: ";
+            for (unsigned int j = 0; j<kauguste_maatriks[i].size(); j++) {
+                cout << kauguste_maatriks[i][j] << ", ";
+            }
+        }
+    }
+    else {
+        cout << "Faili ei leitud!" << endl;
     }
 }
 
